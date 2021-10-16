8eadaaf5e0e2c7ea60026e5170849665d03a133e
Christopher Reid <reidchristopher01@gmail.com>
2021-10-16 16:04:55 -0400

    Fixed Error

--- Learning Day 2.py
+++ Learning Day 2.py
@@ -3,5 +3,5 @@ print('This is the second day of learning')
 
 #This is day 2 learning for me on 10/16/2021
 
-Print('Hi Chris, this is a test to github')
+print('Hi Chris, this is a test to github')
 
