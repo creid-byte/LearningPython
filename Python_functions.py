c27482a9aad857eca4f989b715695c32329bf07c
Christopher Reid <reidchristopher01@gmail.com>
2021-10-18 08:52:57 -0400

    Functions updates

--- Python_functions.py
+++ Python_functions.py
@@ -16,17 +16,34 @@ def good_morning(name='User', morning_person= True):
 
 print(good_morning("Chris", False))
 
-print('\n')
 
+print('\n')
 def print_info(name, age, email):
         print(name + ' is ' + str(age) + '. Reached at ' + email)
 
 print_info('chris', 13, 'chris@gmail')
 
-print('\n')
 
+
+print('\n')
 list = [['chris', 13, 'chris@gmail'],
         ['pixie', 8, 'pixie@gmail']]
 
 for l in list:
     print_info(*l)
+
+
+
+print('\n')
+
+def greet(person, first_time=False):
+    if first_time:
+        return 'welcome'
+    return 'hello, ' + person
+
+def greet_all(people):
+    for person in people:
+        print(greet(person))
+
+friends = ['chris','pixie', 'ron']
+greet_all(friends)
